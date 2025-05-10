756fdebba07b27ff263e2d0a99dbd8929c68e1b0
MeTheOo <leo.the.nerd@gmail.com>
2025-05-10 23:19:09 +0800

    Logic Update
    - Converted some video codecs that cannot be played by ios
    - Fixed video loop logic

--- client02_new/Code_Client/Game/Classes/GamePrecedure.cpp
+++ client02_new/Code_Client/Game/Classes/GamePrecedure.cpp
@@ -1866,7 +1866,7 @@ void GamePrecedure::playMovie(std::string pageName, std::string fileName, int is
 #endif
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
 	fileName = "Video/" + fileName;
-    libOS::getInstance()->playMovie(fileName.c_str());
+    libOS::getInstance()->playMovie(fileName.c_str(), isLoop);
 #endif
 	MainFrame::getInstance()->addMovie(pageName, fileName, isLoop, autoScale);
 }
