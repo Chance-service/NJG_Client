35808554dc92ea97da99f0f1d698d6924b79a1d2
MeTheOo <leo.the.nerd@gmail.com>
2025-06-02 13:59:47 +0800

    Changed some settings to local build env

--- client02_new/Code_Client/Game/proj.android/Ndk-debug_clean.bat
+++ client02_new/Code_Client/Game/proj.android/Ndk-debug_clean.bat
@@ -1,7 +1,7 @@
 cd /d %~dp0
 rd /s /q .\obj
 rd /s /q .\libs
-D:\DevToolChain\android-ndk-r8e\ndk-build -C ./ clean NDK_DEBUG=1 "NDK_MODULE_PATH=../../;../../../Code_Core;../../../Code_Core/cocos2dx/platform/third_party/android/prebuilt" "APP_OPTIM=debug" "APP_CPPFLAGS=-frtti -DCOCOS2D_DEBUG=1 -fexceptions -DANDROID -D_DEBUG"
+C:\DevToolChain\android-ndk-r11c\ndk-build -C ./ clean NDK_DEBUG=1 "NDK_MODULE_PATH=../../;../../../Code_Core;../../../Code_Core/cocos2dx/platform/third_party/android/prebuilt" "APP_OPTIM=debug" "APP_CPPFLAGS=-frtti -DCOCOS2D_DEBUG=1 -fexceptions -DANDROID -D_DEBUG"
 
 
 pause
--- client02_new/Code_Client/Game/proj.android/buildReleaseGameSO.bat
+++ client02_new/Code_Client/Game/proj.android/buildReleaseGameSO.bat
@@ -1,6 +1,6 @@
 echo sh build_native.sh all
 cd /d %~dp0
 
-D:\DevToolChain\android-ndk-r8e\ndk-build -j 4 -C ./ -B NDK_DEBUG=0 "NDK_MODULE_PATH=../../;../../../Code_Core;../../../Code_Core/cocos2dx/platform/third_party/android/prebuilt" "APP_OPTIM=release" "APP_CPPFLAGS=-frtti -DCOCOS2D_DEBUG=0 -fexceptions -O3 -DANDROID -Wno-error=format-security"
+C:\DevToolChain\android-ndk-r11c\ndk-build -j 4 -C ./ -B NDK_DEBUG=0 "NDK_MODULE_PATH=../../;../../../Code_Core;../../../Code_Core/cocos2dx/platform/third_party/android/prebuilt" "APP_OPTIM=release" "APP_CPPFLAGS=-frtti -DCOCOS2D_DEBUG=0 -fexceptions -O3 -DANDROID -Wno-error=format-security"
 
 pause
